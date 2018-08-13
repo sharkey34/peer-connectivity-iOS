2fe635c198cf49dd7fa17c58ff91db9bd1e100d9
Eric Sharkey <eisharkey@fullsail.student.edu>
2018-08-13 19:26:09 -0400

    Initial Commit

---
+++ SharkeyEric_5.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SharkeyEric_5.2
+//
+//  Created by Eric Sharkey on 8/13/18.
+//  Copyright © 2018 Eric Sharkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
