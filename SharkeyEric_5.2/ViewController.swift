748a59a7efab918f6e04654fd04c2fa61e8dabf6
Eric Sharkey <eisharkey@fullsail.student.edu>
2018-08-13 20:12:36 -0400

    Added browser delegate methods and session delegate methods.

--- SharkeyEric_5.2/ViewController.swift
+++ SharkeyEric_5.2/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
+    
+    var session: MCSessionState!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK - MCBrowserViewControllerDelegate
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        <#code#>
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        <#code#>
+    }
+    
+    
+    // MARK - MCSessionDelegate callbacks
+    
+    // Remote peer changed state.
+    public func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState){
+        
+    }
+    
+    
+    // Received data from remote peer.
+    public func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID){
+        
+    }
+    
+    
+    // Received a byte stream from remote peer.
+    public func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID){
+        
+    }
+    
+    
+    // Start receiving a resource from remote peer.
+    public func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress){
+        
+    }
+    
+    
+    // Finished receiving a resource from remote peer and saved the content
+    // in a temporary location - the app is responsible for moving the file
+    // to a permanent location within its sandbox.
+    public func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?){
+        
+    }
 }
 
