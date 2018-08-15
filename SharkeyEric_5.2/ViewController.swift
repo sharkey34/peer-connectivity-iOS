effb161ab02e076ce50ab639a2e2a435de370427
Eric Sharkey <eisharkey@fullsail.student.edu>
2018-08-15 14:34:06 -0400

    Added the timer and sent data when the play button is selected.

--- SharkeyEric_5.2/ViewController.swift
+++ SharkeyEric_5.2/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     @IBOutlet weak var user1Label: UILabel!
     @IBOutlet weak var vsLabel: UILabel!
     @IBOutlet weak var navItem: UINavigationItem!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var playSelectedLabel: UILabel!
     
     // Creating variables.
     var session: MCSession!
@@ -37,7 +39,10 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     var win = 0
     var draw = 0
     var loss = 0
-    var selectedImage: UIImage!
+    var selectedImage: Int!
+    var timer = Timer()
+    var timerCounter = 3
+    var playCounter = 0
     
     let serviceId = "sharkeyEric-52"
 
@@ -80,38 +85,55 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     
     // Remote peer changed state.
     public func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState){
-        switch state {
+        
+        DispatchQueue.main.async {
+            switch state {
             // Maybe add activity indicator.
-        case .connected:
-            navItem.title = "Connected"
-            user1Label.text = UIDevice.current.name
-            user2Label.text = peerID.displayName
-            playButton.setTitle("Play", for: .normal)
-            roShamBoLabel.text = nil
-        case .notConnected:
-            navItem.title = "Disconnected"
-            
-        //MARK: Add alert notifying user of disconnection.
-        case .connecting:
-            navItem.title = "Connecting..."
+            case .connected:
+                self.navItem.title = "Connected"
+                self.user1Label.text = UIDevice.current.name
+                self.user2Label.text = peerID.displayName
+                self.playButton.setTitle("Play", for: .normal)
+                self.roShamBoLabel.text = nil
+            case .notConnected:
+                self.navItem.title = "Disconnected"
+                
+            //MARK: Add alert notifying user of disconnection.
+            case .connecting:
+                self.navItem.title = "Connecting..."
+            }
         }
-        
-        
-        
-        
     }
     
     
     // Received data from remote peer.
     public func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID){
-
+        
         //MARK: Start 3 second timer with selector function.
+        
+        if let data: String = String(data: data, encoding: String.Encoding.utf8){
+            DispatchQueue.main.async {
+                if data == "Play"{
+                    self.playCounter += 1
+                    self.playSelectedLabel.text = "\(peerID.displayName) is ready..."
+                    if self.playCounter == 2 {
+                        
+                        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.checkImage), userInfo: nil, repeats: true)
+                        
+                        self.playCounter = 0
+                    }
+                } else{
+                    print(data)
+                    self.userChoiceImages[1].image = self.rpsImageViews[Int(data)!].image
+                }
+            }
+        }
     }
     
     
     // Received a byte stream from remote peer.
     public func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID){
-        print("ImageRecieved")
+        
     }
     
     
@@ -142,16 +164,18 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
         }
         
         for label in wdlLables{
-            label.text = ""
+            label.text = nil
         }
         
-        resultLabel.text = ""
-        user1Label.text = ""
-        user2Label.text = ""
-        vsLabel.text = ""
+        resultLabel.text = nil
+        user1Label.text = nil
+        user2Label.text = nil
+        vsLabel.text = nil
+        counterLabel.text = nil
+        playSelectedLabel.text = nil
         
         for label in tallyCollection{
-            label.text = ""
+            label.text = nil
         }
         
         topView.backgroundColor = UIColor.white
@@ -165,48 +189,64 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessi
     switch selectedView.tag {
     case 0:
         userChoiceImages[0].image = #imageLiteral(resourceName: "rock")
-        selectedImage = #imageLiteral(resourceName: "rock")
+        selectedImage = 0
     case 1:
         userChoiceImages[0].image = #imageLiteral(resourceName: "paper")
-        selectedImage = #imageLiteral(resourceName: "paper")
+        selectedImage = 1
     case 2:
         userChoiceImages[0].image = #imageLiteral(resourceName: "scissors")
-        selectedImage = #imageLiteral(resourceName: "scissors")
+        selectedImage = 2
     default:
         print("Incorrect image tag")
     }
     
-    
+//    guard let index = selectedImage else {return}
+//
+//    if let text = Data.init(base64Encoded: String(index)) {
+//
+//        do{
+//            try session.send(text, toPeers: session.connectedPeers, with: MCSessionSendDataMode.reliable)
+//        } catch{
+//            print(error.localizedDescription)
+//        }
+//    }
     
     }
     @IBAction func playTapped(_ sender: UIButton) {
+        playCounter += 1
         
-        guard let buttonText = playButton.titleLabel?.text else {return}
-//     let test = Data.init(base64Encoded: buttonText)
-//        buttonText.data(using: String.Encoding.utf8)
-//
-//        if let path = Bundle.main.path(forResource: "rock", ofType: "png"){
-//            let url = URL(fileURLWithPath: path)
-//
-//                session.sendResource(at: url, withName: "rock.png", toPeer: peerId, withCompletionHandler: nil)
-//        }
-        
-     
-        
-        if let text = Data.init(base64Encoded: buttonText) {
+        if playCounter == 2 {
             
-            do{
-                try session.send(text, toPeers: session.connectedPeers, with: MCSessionSendDataMode.reliable)
-            } catch{
-                print(error.localizedDescription)
-            }
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.checkImage), userInfo: nil, repeats: true)
+            
+            playCounter = 0
         }
+            guard let buttonText = playButton.titleLabel?.text else {return}
         
+            if let text = buttonText.data(using: String.Encoding.utf8) {
+        
+                do{
+                    try session.send(text, toPeers: session.connectedPeers, with: MCSessionSendDataMode.reliable)
+                } catch{
+                    print(error.localizedDescription)
+                }
+            }
     }
     
     @objc func checkImage (){
-    
+      
+        if timerCounter > 0 {
+            timerCounter -= 1
+            counterLabel.text = "\(timerCounter)"
+        } else {
+            // MARK: When Timer hits 0 check the images and display.
+            
+            
+            
+            timerCounter = 3
+            timer.invalidate()
+            counterLabel.text = nil
+        }
     }
-    
 }
 
